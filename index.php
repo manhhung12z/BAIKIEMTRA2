c93ec4bcc9d9a80436af100b5510b04370017f66
strong.dz <dang98788@gmail.com>
2025-11-06 19:55:15 +0700

    xoá file

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$controller = ucfirst($_REQUEST['controller'] ?? 'home') .'Controller';
-$actionName =$_REQUEST['action'] ?? 'index';
-require "controller/{$controller}.php";
-$controllerobject  = new $controller();
-// sẽ xử lí đến method_exists
-$controllerobject ->$actionName();
-?>
